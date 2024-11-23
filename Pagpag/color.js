dfdd513851e3bb70630b860582350284ddd38576
jam-ian <jamjamelises@gmail.com>
2024-11-23 21:25:50 +0800

    Delete Pagpag directory

--- Pagpag/color.js
+++
@@ -1,7 +0,0 @@
-
-let box = document.querySelector('#ba');
-if(box.body.style.backgroundColor == "aqua"){
-    box.addEventListener('click' , (e) =>{
-        box.style.backgroundColor = "green";
-})
-}
